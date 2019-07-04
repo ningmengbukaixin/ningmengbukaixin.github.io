d5db954a7cfbb77b7e3bbefaaf550749d983342f
王鑫 <534289894@qq.com>
2019-07-04 08:54:01 +0800

    1111

---
+++ src/main/java/com/wangxin/demo/controller/CommonController.java
@@ -0,0 +1,16 @@
+package com.wangxin.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class CommonController {
+
+    @RequestMapping({"index","","/"})
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        return mv;
+    }
+}
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,17 @@
+<%--
+
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%
+    String path = request.getContextPath();
+    application.setAttribute("path", request.getContextPath());
+%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+页面装修中~~~~~~~~~~~
+</body>
+</html>
